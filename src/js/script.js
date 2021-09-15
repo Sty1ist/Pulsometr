0d479bf015bfb9584ffef0cf5c920eaaf2d93935
Instinct <imbalancerm@gmail.com>
2021-09-15 23:02:12 +0300

    добавил стрелки к слайдеру и начал их стилизациию

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,13 @@
 $(document).ready(function () {
     $('.carousel__inner').slick({
+        speed: 1300,
+        slidesToShow: 1,
+        centerMode: true,
+        variableWidth: true,
+        autoplay: true,
+        autoplaySpeed: 2800,
+        prevArrow: '<button type="button" class="slick-prev"><img src="../img/Carousel/icons/left.png"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="../img/Carousel/icons/right.png"></button>'
     });
 });
 
