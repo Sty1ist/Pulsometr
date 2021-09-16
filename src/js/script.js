2047a6c2a991a8352889669217d77d785d28b82b
Instinct <imbalancerm@gmail.com>
2021-09-16 22:56:26 +0300

    начинаю разработку 5й секции/ табы на сайте

--- src/js/script.js
+++ src/js/script.js
@@ -5,9 +5,19 @@ $(document).ready(function () {
         centerMode: true,
         variableWidth: true,
         autoplay: true,
-        autoplaySpeed: 2800,
-        prevArrow: '<button type="button" class="slick-prev"><img src="../img/Carousel/icons/left.png"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="../img/Carousel/icons/right.png"></button>'
+        autoplaySpeed: 4800,
+        prevArrow: '<button type="button" class="slick-prev"><img src="img/Carousel/icons/left.png"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="img/Carousel/icons/right.png"></button>',
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                    dots: true,
+                    arrows: false
+                }
+
+            }
+        ]
     });
 });
 
