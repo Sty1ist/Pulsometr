133411623f342dc1f8a7b7c31debe6c0d2227684
Instinct <imbalancerm@gmail.com>
2021-09-18 23:47:18 +0300

    сделал адаптацию для слайдера под все размеры

--- src/js/script.js
+++ src/js/script.js
@@ -2,21 +2,23 @@ $(document).ready(function () {
     $('.carousel__inner').slick({
         speed: 1300,
         slidesToShow: 1,
-        centerMode: true,
-        variableWidth: true,
+        // centerMode: true,
+        // variableWidth: true,
         autoplay: true,
         autoplaySpeed: 4800,
         prevArrow: '<button type="button" class="slick-prev"><img src="img/Carousel/icons/left.png"></button>',
         nextArrow: '<button type="button" class="slick-next"><img src="img/Carousel/icons/right.png"></button>',
         responsive: [
             {
-                breakpoint: 992,
+                breakpoint: 991,
                 settings: {
                     dots: true,
-                    arrows: false
-                }
+                    arrows: false,
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                },
 
-            }
+            },
         ]
     });
 
