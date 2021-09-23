981ece02b8793ac52719edb9ad6e260dc7408cbd
Instinct <imbalancerm@gmail.com>
2021-09-23 22:36:00 +0300

    сделал 3 модальных окна

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+$(document).ready(function () {
     $('.carousel__inner').slick({
         speed: 1300,
         slidesToShow: 1,
@@ -17,11 +17,11 @@ $(document).ready(function() {
                 slidesToScroll: 1,
             },
 
-        }, ]
+        },]
     });
 
 
-    $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+    $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function () {
         $(this)
             .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
             .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
@@ -44,8 +44,8 @@ $(document).ready(function() {
     // })
 
     function toggleSlide(item) {
-        $(item).each(function(i) {
-            $(this).on('click', function(e) {
+        $(item).each(function (i) {
+            $(this).on('click', function (e) {
                 e.preventDefault();
                 $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
                 $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
@@ -59,6 +59,15 @@ $(document).ready(function() {
 
     //modal
 
-    $('[data-modal=consultation]').fadeOut();
+    $('[data-modal=consultation]').on('click', function () {
+        $('.overlay,  #consultation').fadeIn('slow');
+    });
 
+    $('.modal__close').on('click', function (){
+        $('.overlay, #consultation, #thanks, #order').fadeOut('fast');
+    });
+
+    $('.button_mini').on('click', function() {
+        $('.overlay, #order').fadeIn('slow');
+    });
 });
