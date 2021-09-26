9f1cfe59c17048d922237f7e4578600680bf26d8
Instinct <imbalancerm@gmail.com>
2021-09-27 00:32:06 +0300

    добавил скрипт для плавной прокрутки (pageup)

--- src/js/script.js
+++ src/js/script.js
@@ -132,4 +132,11 @@ $(document).ready(function () {
             $('.pageup').fadeOut();
       }
     });
+    
+    let top = Math.max(document.body.scrollTop,document.documentElement.scrollTop);
+	if(top > 0) {
+		window.scrollBy(0,-100);
+		t = setTimeout('up()',20);
+	} else clearTimeout(t);
+	return false;
 });
