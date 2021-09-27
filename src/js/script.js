6e5e21810a42e57648981d41544a006816179c6a
Instinct <imbalancerm@gmail.com>
2021-09-27 04:38:27 +0300

    настроил анимацию для коментариев(fadeUP) сработывает при скроле к секции

--- src/js/script.js
+++ src/js/script.js
@@ -133,10 +133,12 @@ $(document).ready(function () {
       }
     });
     
-    let top = Math.max(document.body.scrollTop,document.documentElement.scrollTop);
-	if(top > 0) {
-		window.scrollBy(0,-100);
-		t = setTimeout('up()',20);
-	} else clearTimeout(t);
-	return false;
+    // let top = Math.max(document.body.scrollTop,document.documentElement.scrollTop);
+	// if(top > 0) {
+	// 	window.scrollBy(0,-100);
+	// 	t = setTimeout('up()',20);
+	// } else clearTimeout(t);
+    // return false;
+    
+    new WOW().init();
 });
